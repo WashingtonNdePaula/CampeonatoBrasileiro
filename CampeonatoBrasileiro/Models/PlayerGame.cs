81ed254a63ff3d104ae5e975b9888d52ffc0102e
WashingtonNdePaula <starswas@outlook.com>
2017-11-20 21:17:37 -0200

    A

---
+++ CampeonatoBrasileiro/Models/PlayerGame.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CampeonatoBrasileiro.Models
+{
+    public class PlayerGame : PlayerSeason
+    {
+        public int? Jersey { get; set; }
+        public bool? Captain { get; set; }
+        public bool? Suspension { get; set; }
+        public string SuspensionReason { get; set; }
+        public int? FanDuelSalary { get; set; }
+        public int? DraftkingsSalary { get; set; }
+        public int? MondogoalSalary { get; set; }
+        public string FanDuelPosition { get; set; }
+        public string DraftKingsPosition { get; set; }
+        public string YahooPosition { get; set; }
+        public string MondogoalPosition { get; set; }
+        public string InjuryStatus { get; set; }
+        public string InjuryBodyPart { get; set; }
+        public string InjuryNotes { get; set; }
+        public string InjuryStartDate { get; set; }
+        public int? GameId { get; set; }
+        public int? OpponentId { get; set; }
+    }
+}
---
+++ CampeonatoBrasileiro/Models/TeamGame.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CampeonatoBrasileiro.Models
+{
+    public class TeamGame : TeamSeason
+    {
+        public int? GameId { get; set; }
+        public int? OpponentId { get; set; }
+        public string Opponent { get; set; }
+        public string Day { get; set; }
+        public string Datetime { get; set; }
+        public string HomeOrAway { get; set; }
+        public bool? IsGameOver { get; set; }
+        public int? GlobalGameId { get; set; }
+        public int? GlobalOpponentId { get; set; }
+    }
+}
